fc8933fb6b9d7ca40bf3b27e9cff4e35fe3b499c
kulikov <kulikov.webfree@gmail.com>
2024-12-20 23:55:49 +0300

    Lesson 3 commit 04

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,14 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+let heightNumber
+function findLargest(num1, num2, num3) {
+    
+    if (num1 > num2 && num1 > num3) {
+        heightNumber = "Heigest number: " + num1;
+    } else if (num2 > num1 && num2 > num3) {
+        heightNumber = "Heigest number: " + num2;
+    } else {
+        heightNumber = "Heigest number: " + num3;
+    }
+    return heightNumber;
+}
