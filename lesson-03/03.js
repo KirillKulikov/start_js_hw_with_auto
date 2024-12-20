865d942eec2165f1fca17799099bd247ea3cd112
kulikov <kulikov.webfree@gmail.com>
2024-12-20 23:59:41 +0300

    Lesson 3 commit 05

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -4,11 +4,11 @@ let heightNumber
 function findLargest(num1, num2, num3) {
     
     if (num1 > num2 && num1 > num3) {
-        heightNumber = "Heigest number: " + num1;
+        heightNumber = num1;
     } else if (num2 > num1 && num2 > num3) {
-        heightNumber = "Heigest number: " + num2;
+        heightNumber = num2;
     } else {
-        heightNumber = "Heigest number: " + num3;
+        heightNumber = num3;
     }
     return heightNumber;
 }
