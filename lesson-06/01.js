47c6583731f6b58108479d3dea918acb9ea3294e
kulikov <kulikov.webfree@gmail.com>
2025-01-09 23:19:16 +0300

    Lessons 06 commit 01

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -17,3 +17,31 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+// получение элементов
+const sliderPrevBtn = document.getElementById('prev-button');
+const sliderNextBtn = document.getElementById('next-button');
+const sliderImg = document.getElementById('web-tech-image');
+
+let counterValue = 0; // исходное значение счетчика
+
+function updateCurrentValueImage() { 
+  return sliderImg.src = WEB_TECH_IMAGES[counterValue]; 
+} 
+  console.log(updateCurrentValueImage());
+  
+  sliderPrevBtn.addEventListener('click', function() {
+    counterValue = (counterValue === 0) ? WEB_TECH_IMAGES.length - 1 : counterValue - 1;
+    
+    updateCurrentValueImage()
+  })
+
+
+ sliderNextBtn.addEventListener('click', function() {
+  counterValue = (counterValue === WEB_TECH_IMAGES.length - 1) ? 0 : counterValue + 1;
+  
+    updateCurrentValueImage()
+})
+
+// показ исходного изображения
+updateCurrentValueImage()
