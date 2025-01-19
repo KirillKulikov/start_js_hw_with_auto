5c02e2446e0ba80dbb92cd6da360fa7861f6411f
kulikov <kulikov.webfree@gmail.com>
2025-01-19 12:11:10 +0300

    Lessons 07 commit 01

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -6,4 +6,22 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+const str = "hello, kirill kulikov";
+
+function capitalizeWords(strCapitalize) {
+  let newStr = strCapitalize[0].toUpperCase();
+  // for (let index = 0; index < strCapitalize.length; index++) {
+  //   newStr = newStr + strCapitalize[index].toUpperCase();
+  // }
+  for (let index = 1; index < strCapitalize.length; index++) {
+    if (strCapitalize[index - 1] === " ") {
+      newStr = newStr + strCapitalize[index].toUpperCase()
+    } else {
+      newStr += strCapitalize[index];
+    }
+  }
+
+  return newStr;
+}
+
+console.log(capitalizeWords(str));
